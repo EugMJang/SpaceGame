ee0df0b65e6fb907f824b396ab99e329fb61cdf7
Eugene Jang <eug.jang123@gmail.com>
2021-06-28 23:06:22 -0500

    Added bullets that kill enemies, MVP completed

--- game.py
+++ game.py
@@ -19,10 +19,10 @@ def main():
     enemies = pygame.sprite.Group()
     enemies.add(Enemy(main_player))
 
-    healthbars = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
 
     SPAWN_EVENT = pygame.USEREVENT + 1
-    pygame.time.set_timer(SPAWN_EVENT, 1000)
+    pygame.time.set_timer(SPAWN_EVENT, 500)
     while running:
         game_clock.tick(60)
         screen.fill((0, 0, 0))
@@ -31,13 +31,22 @@ def main():
         player.update()
         enemies.draw(screen)
         enemies.update()
+        bullets.draw(screen)
+        bullets.update()
         pygame.display.flip()
+
+        collisions = pygame.sprite.groupcollide(bullets, enemies, True, False)
+        if collisions:
+            for enemy in collisions.values():
+                enemy[0].health -= 50
 
         for event in pygame.event.get():
             if event.type == SPAWN_EVENT:
                 enemies.add(Enemy(main_player))
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bullets.add(Bullet(main_player))
 
 class MainPlayer(pygame.sprite.Sprite):
     def __init__(self):
@@ -95,23 +104,59 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         which_side = random.randint(0, 1000)
         if which_side % 2 == 0:
-            self.rect.center = (-10, random.randint(10, height - 10))
+            if which_side % 4 == 0:
+                self.rect.center = (-10, random.randint(10, height / 2 - 75))
+            else:
+                self.rect.center = (-10, random.randint(height / 2 + 75, height + 10))
         else:
-            self.rect.center = (width + 10, random.randint(10, height - 10))
+            if which_side % 4 == 3:
+                self.rect.center = (width + 10, random.randint(10, height / 2 - 75))
+            else:
+                self.rect.center = (width + 10, random.randint(height / 2 + 75, height + 10))
 
         self.health_bar = HealthBar((self.rect.centerx, self.rect.bottom + 10), self.size, (0, 0, 0), (175, 0, 0))
     
     def update(self):
         self.health_bar.draw(self.health, (self.rect.centerx, self.rect.bottom + 10))
-        if (not pygame.sprite.collide_mask(self, self.player)):
+        if not pygame.sprite.collide_mask(self, self.player):
             self.move_to_player()
         else:
             self.player.health -= 0.25
+        if self.health <= 0:
+            self.kill()
+            del self
 
     def move_to_player(self):
         rel_pos = rel_x, rel_y = self.rect.centerx - self.player.rect.centerx, self.rect.centery - self.player.rect.centery
         distance_to_player = int(dist([0, 0], rel_pos))
         self.rect = self.rect.move(self.speed * rel_x / distance_to_player, self.speed * rel_y / distance_to_player)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, player):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.size = 10
+        
+        self.speed = 7
+
+        self.image = pygame.Surface((self.size, self.size))
+        pygame.draw.circle(self.image, (200, 200, 200), (self.size / 2, self.size / 2), self.size / 2)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = player.rect.center
+        
+        self.destx, self.desty = pygame.mouse.get_pos()
+        rel_pos = self.rel_x, self.rel_y = self.destx - self.rect.centerx, self.desty - self.rect.centery
+        self.distance_to_player = int(dist([0, 0], rel_pos))
+
+    def update(self):
+        self.move_in_direction()
+        if self.rect.center > (width, height) or self.rect.center < (0, 0):
+            self.kill()
+            del self
+
+    def move_in_direction(self):
+        self.rect = self.rect.move(self.speed * self.rel_x / self.distance_to_player, self.speed * self.rel_y / self.distance_to_player)
 
 class HealthBar():
     def __init__(self, pos, width, color, health_color):
